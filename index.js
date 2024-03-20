3bb5d9277d20d9504fa3913102376a80f6f0652e
iankuria668 <iankuria668@gmail.com>
2024-03-20 22:44:00 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function   saturdayFun() {
+let activity;
+if ((activity = arguments[0]) === undefined) {
+    activity = "roller-skate";
+}
+return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork() {
+    let activity;
+    if ((activity = arguments[0]) === undefined) {
+        activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`;
+}
+
+const wrapAdjective = function(Flair="*"){
+    return function(adjective="special"){
+        return `You are ${Flair}${adjective}${Flair}!`;
+    }
+    return 
+}
